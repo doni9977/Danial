67dd8a59884f550c387a207f6a2f2904f34a4019
danialmarat <doni.danialmarat20066@gmail.com>
2025-03-28 15:38:04 +0500

    solution for problem 1

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,4 +1,33 @@
 package src;
 
+import java.util.Scanner;
+
 public class Lab1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++){
+            array[i] = sc.nextInt();
+        }
+
+        int min = findMin(array,n);
+        System.out.println(min);
+
+        sc.close();
+    }
+
+    public static int findMin(int[] array, int n){
+        if (n == 1) {
+            return array[0];
+        }
+        int minOfRest = findMin(array, n - 1);
+
+        if (minOfRest < array[n-1]) {
+            return minOfRest;
+        } else {
+            return array[n-1];
+        }
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,4 +1,4 @@
-package PACKAGE_NAME;
+package src;
 
 public class main {
 }
